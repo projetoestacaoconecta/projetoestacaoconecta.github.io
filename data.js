0322e5b31eeddd81bd05330ba28f0f2407cb1be2
Jeezyel <jeezyelpereira@gmail.com>
2024-04-24 00:25:26 -0300

    finalizado o Preenchimento dos dados

--- data.js
+++ data.js
@@ -1,88 +1,21 @@
 
 const data = {
   courses: [
-    {
-      id: 'course1',
-      cardType: 'Curso',
-      duration: '3 horas',
-      title: 'Curso de HTML, CSS e JS',
-      image: './images/fundoitem2.jpg',
-      tags: ['HTML', 'CSS', 'JS'],
-      shortDescription: 'Curso de HTML, CSS e JS voltado para iniciantes e intermediários. Aprenda a criar sites responsivos e dinâmicos com as tecnologias mais utilizadas no mercado.',
-      longDescription: '<p>Curso de HTML, CSS e JS voltado para iniciantes e intermediários. Aprenda a criar sites responsivos e dinâmicos com as tecnologias mais utilizadas no mercado. Aprenda a criar sites responsivos e dinâmicos com as tecnologias mais utilizadas no mercado. Aprenda a criar sites responsivos e dinâmicos com as tecnologias mais utilizadas no mercado.<p>' +
-      `<p>Nesse curso veremos:</p>
-      <ul>
-        <li>Basic HTML structure</li>
-        <li>Styling with CSS</li>
-        <li>Adding interactivity with JavaScript</li>
-      </ul>`,
-      instructors: [
-        {
-          name: 'João Silva',
-          photo: 'images/user.jpg'
-        },
-        {
-          name: 'Maria Souza',
-          photo: 'images/user.jpg'
-        }
-      ],
-      usefulLinks: [
-        {
-          title: 'Turma Google Classroom',
-          url: 'https://classroom.google.com/u/0/c/NjY4ODk2OTU2MDYz',
-          qrCodeImage: './images/qrcode/cursohtml.png'
-        },
-        {
-          title: 'Link 2',
-          url: 'https://www.google.com',
-          qrCodeImage: 'https://picsum.photos/200'
-        }
-      ],
-      local: 'Bloco C, sala 101',
-      subscribeButton: 'https://classroom.google.com/u/0/c/NjY4ODk2OTU2MDYz'
-    }
   ],
   oficinas: [
-    {
-      id: 'oficinas1',
-      cardType: 'oficinas',
-      duration: '3 horas',
-      title: 'workshop para modelagem usando BPMN',
-      image: 'images/BPMN/BPMN.PNG',
-      tags: ['Gestão de fluxo', 'velocidade', 'Gestão de projetos'],
-      shortDescription: 'Nesse curso voce vai o que e e pra que server o BPMN.',
-      longDescription: 'Nesse curso você vai aprender para que server o BPMN.'
-      ,
-      instructors: [
-        {
-          name: 'Jeezyel Pereira Costa',
-          photo: 'images/user.jpg'
-        },
-        {
-          name: 'Leandro Guimarães Garcia',
-          photo: 'images/user.jpg'
-        }
-      ],
-      usefulLinks: [
-        {
-          title: 'Turma Google sala de aula',
-          url: 'https://classroom.google.com/c/NjY4ODkzNTk0NDQz',
-          qrCodeImage: 'images/BPMN/qr-code BPMN.PNG'
-        },
-      ],
-      local: 'ex: Bloco C, sala 101 colocar depos', // comfirma o local
-      subscribeButton: 'https://classroom.google.com/u/0/c/NjY4ODk2OTU2MDYz'//colocar o link do portal quendo estiver pronto
-    }, 
     /////////////////////////////////////////////////////////////////////////////////////////////////////////
+    
+  ],
+  workshops: [
     {
-      id: 'oficinas1',
-      cardType: 'oficinas',
+      id: 'workshop1',
+      cardType: 'Workshop',
       duration: '3 horas',
-      title: 'Oficina de Design e Prototipação com Figma',
+      title: 'Workshop de Design e Prototipação com Figma',
       image: 'images/FIGMA/FIGMA.PNG',
       tags: ['Prototipação', 'Interfaces', 'Experiência de Usuário ', 'Criatividade' , 'Inteligência'],
-      shortDescription: 'O curso irá ensinar os seus cursantes a fazer uma protoptipagem funcional para o Figma.',
-      longDescription: 'O objetivo final do curso é montar um protótipo executável e funcional. Esse protótipo será uma poderosa ferramenta para demonstrar a clientes e gerentes interessados na criação de uma plataforma digital de vendas. Prepare-se para explorar o Figma e elevar suas habilidades de design para um novo patamar!'
+      shortDescription: 'O Workshop irá ensinar os seus cursantes a fazer uma protoptipagem funcional para o Figma.',
+      longDescription: 'O objetivo final do Workshop é montar um protótipo executável e funcional. Esse protótipo será uma poderosa ferramenta para demonstrar a clientes e gerentes interessados na criação de uma plataforma digital de vendas. Prepare-se para explorar o Figma e elevar suas habilidades de design para um novo patamar!'
       ,
       instructors: [
         {
@@ -108,24 +41,223 @@ const data = {
       local: 'local: Câmpus Palmas, Bloco B.', // comfirma o local
       subscribeButton: 'https://classroom.google.com/u/0/c/NjY4ODk2OTU2MDYz'//colocar o link do portal quendo estiver pronto
     },
-  ],
-  workshops: [
-    {
-      id: 'workshop1',
-      cardType: 'Workshop',
-      duration: '2 horas',
-      title: 'Workshop de React',
-      tags: ['React', 'Frontend', 'JavaScript'],
-      shortDescription: 'Aprenda a criar aplicações web com React, uma das bibliotecas mais populares para desenvolvimento frontend.',
-    },
+    ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     {
       id: 'workshop2',
       cardType: 'Workshop',
       duration: '3 horas',
-      title: 'Workshop de Node.js',
-      tags: ['Node.js', 'Backend', 'JavaScript'],
-      shortDescription: 'Aprenda a criar aplicações backend com Node.js, uma das tecnologias mais utilizadas no mercado.',
-    }
+      title: 'Workshop Básico de HTML5/CSS3/JS',
+      image: 'images/htmlCSS/html e css .jpg',
+      tags: ['Iniciante', 'JS', 'CSS', 'HTML'],
+      shortDescription: 'Descubra o mundo do desenvolvimento de websites com nosso Workshop introdutório, projetado especialmente para quem está começando. Neste Workshop, você aprenderá a estruturar páginas com HTML, estilizá-las com CSS e adicionar interatividade com JavaScript.',
+      longDescription: 'Descubra o mundo do desenvolvimento de websites com nosso Workshop introdutório, projetado especialmente para quem está começando. Neste Workshop, você aprenderá a estruturar páginas com HTML, estilizá-las com CSS e adicionar interatividade com JavaScript.',
+
+      instructors: [
+        {
+          name: 'Luiz Eduardo woicik',
+          photo: 'images/user.jpg'
+        },
+        {
+          name: 'Andre Barreira',
+          photo: 'images/user.jpg'
+        },
+        {
+          name: 'Alfredo de Souza Aguiar Neto',
+          photo: 'images/user.jpg'
+        }
+      ],
+      usefulLinks: [
+        {
+          title: 'link de convite sala classroom',
+          url: 'https://classroom.google.com/c/NjY4ODk2OTU2MDYz?cjc=gfjni7n',
+          qrCodeImage: 'images/htmlCSS/qr-codeHTMLCSSJS.PNG'
+        },
+      ],
+      local: 'ainda não decidido.', // comfirma o local
+      subscribeButton: 'https://classroom.google.com/u/0/c/NjY4ODk2OTU2MDYz'//colocar o link do portal quendo estiver pronto
+    },
+    ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    {
+      id: 'workshop3',
+      cardType: 'Workshop',
+      duration: '3 horas',
+      title: 'Aprendendo a compartilhar seu projeto com git e github!',
+      image: 'images/GIT/GIT.PNG',
+      tags: ['Git', 'Github', 'JavaScript', 'Share', 'Compartilhar', 'Versionar'],
+      shortDescription: 'Você irá aprender a como pode compartilhar e versionar seus projetos mais legais dentro da rede social dos desenvolvedores, usando Git e Github!',
+      longDescription: 'Você irá aprender a como pode compartilhar e versionar seus projetos mais legais dentro da rede social dos desenvolvedores, usando Git e Github!',
+
+      instructors: [
+        {
+          name: 'João Victor Póvoa França',
+          photo: 'images/user.jpg'
+        },
+        {
+          name: 'Vitória Gabriela',
+          photo: 'images/user.jpg'
+        }
+      ],
+      usefulLinks: [
+        {
+          title: 'github JoaoIto',
+          url: 'https://github.com/JoaoIto',
+          qrCodeImage: 'images/GIT/qr-codeGITJ.PNG'
+        },
+        {
+          title: 'github Vitoria',
+          url: 'https://github.com/viihgabs',
+          qrCodeImage: 'images/GIT/qr-codeGITV.PNG'
+        },
+      ],
+      local: 'ainda não decidido.', // comfirma o local
+      subscribeButton: 'https://classroom.google.com/u/0/c/NjY4ODk2OTU2MDYz'//colocar o link do portal quendo estiver pronto
+    },
+    ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    {
+      id: 'workshop4',
+      cardType: 'Workshop',
+      duration: '3 horas',
+      title: 'Workshop de Ferramentas de Edição: Word, Excel, Powerpoint e Canva',
+      image: 'images/FERRAMENTAS/FERRAMENTAS.PNG',
+      tags: ['Word', 'Excel', 'Powerpoint', 'Canva'],
+      shortDescription: 'Este Workshop capacitará os participantes no uso das ferramentas Word, Excel, Powerpoint e Canva, essenciais para a comunicação e apresentação de informações em ambientes acadêmicos e corporativos, aprimorando assim, suas produções e apresentações.',
+      longDescription: 'O Workshop irá apresentar e orientar os participantes sobre o uso das ferramentas mais utilizadas no mundo profissional atualmente: Word, Excel, PowerPoint e Canva. Essas ferramentas desempenham um papel fundamental na comunicação, organização e apresentação de informações em diversos contextos, desde o ambiente acadêmico até o corporativo, e visa capacitar os participantes a dominar as principais funcionalidades dessas ferramentas, fornecendo-lhes habilidades práticas para aprimorar suas produções e apresentações.',
+
+      instructors: [
+        {
+          name: 'Nicole Araujo Muniz',
+          photo: 'images/user.jpg'
+        },
+        {
+          name: 'Álvaro Calebe Souza Ferreira',
+          photo: 'images/user.jpg'
+        },
+        {
+          name: 'Marcelo Alves Leite',
+          photo: 'images/user.jpg'
+        },
+        {
+          name: 'Emily Maria Santos Bessa',
+          photo: 'images/user.jpg'
+        },
+        {
+          name: 'Emanuel Pinheiro Cirqueira',
+          photo: 'images/user.jpg'
+        }
+      ],
+      usefulLinks: [
+        {
+          title: 'Material do Workshop de edição',
+          url: 'https://drive.google.com/drive/folders/14zsyMCyln7hvhjutol6MNqOCcYFTw8NE?usp=sharing',
+          qrCodeImage: 'images/FERRAMENTAS/qr-codeFERRAMENTAS.PNG'
+        },
+      ],
+      local: 'UNITINS - Campus Graciosa, Bloco B, Labin III.', // comfirma o local
+      subscribeButton: 'https://classroom.google.com/u/0/c/NjY4ODk2OTU2MDYz'//colocar o link do portal quendo estiver pronto
+    },
+    ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    {
+      id: 'workshop5',
+      cardType: 'Workshop',
+      duration: '2 horas',
+      title: 'Montagem de Computadores',
+      image: 'images/MONTAGEM/MONTAGEM.PNG',
+      tags: ['Reparo de Computadores', 'Manotemção de Computadores'],
+      shortDescription: 'O workshop "Montagem de Computadores" é uma experiência educativa criada para ensinar aos participantes os fundamentos práticos de como montar um computador desktop do zero. Durante o Workshop, você irá explorar os componentes e principais sistemas operacionais de um computador e aprender boas praticas de montagem.',
+      longDescription: 'O workshop "Montagem de Computadores" é uma experiência educativa criada para ensinar aos participantes os fundamentos práticos de como montar um computador desktop do zero. Durante o Workshop, você irá explorar os componentes e principais sistemas operacionais de um computador e aprender boas praticas de montagem.',
+
+      instructors: [
+        {
+          name: 'Gabriel Alves da Silva',
+          photo: 'images/user.jpg'
+        },
+        {
+          name: 'Felipe Ferreira de Sousa',
+          photo: 'images/user.jpg'
+        },
+        {
+          name: 'Luis Eduardo Noleto de Melo',
+          photo: 'images/user.jpg'
+        },
+      ],
+      usefulLinks: [
+        {
+          title: 'google sala de aula',
+          url: 'https://classroom.google.com/c/NjY4ODk1NTg5Mzgz?cjc=oe6z2xv',
+          qrCodeImage: 'images/MONTAGEM/qr-codeMONTAGEM.PNG'
+        },
+      ],
+      local: 'UNITINS - Bloco B labin 6.', // comfirma o local
+      subscribeButton: 'https://classroom.google.com/u/0/c/NjY4ODk2OTU2MDYz'//colocar o link do portal quendo estiver pronto
+    },
+    ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    {
+      id: 'workshop6',
+      cardType: 'Workshop',
+      duration: '2 horas',
+      title: 'Workshop de boas práticas em suporte de sistemas',
+      image: 'images/BOAS/BOAS.PNG',
+      tags: ['Gerenciamento de incidentes', 'Resolução de problemas', 'Monitoramento de sistemas', 'Segurança da informação'],
+      shortDescription: 'Este Workshop tem como objetivo apresentar as melhores práticas e estratégias para oferecer suporte eficaz a sistemas de software. Durante a sessão, serão exploradas técnicas para identificar, diagnosticar e resolver problemas de maneira rápida e eficiente, proporcionando aos participantes as habilidades necessárias para fornecer suporte de alta qualidade para usuários e clientes.',
+      longDescription: 'Este Workshop tem como objetivo apresentar as melhores práticas e estratégias para oferecer suporte eficaz a sistemas de software. Durante a sessão, serão exploradas técnicas para identificar, diagnosticar e resolver problemas de maneira rápida e eficiente, proporcionando aos participantes as habilidades necessárias para fornecer suporte de alta qualidade para usuários e clientes.',
+
+      instructors: [
+        {
+          name: 'Luiz Cláudio de Oliveira',
+          photo: 'images/user.jpg'
+        },
+        {
+          name: 'Jônatas Pereira de Jesus Daher',
+          photo: 'images/user.jpg'
+        },
+      ],
+      usefulLinks: [
+        {
+          title: 'google sala de aula',
+          url: 'https://drive.google.com/drive/u/3/folders/11WgWUi5jq1UyQBEcqkewnvb0ZY5m3QiL',
+          qrCodeImage: 'images/BOAS/qr-codeBOAS GOOGLE SALA.PNG'
+        },
+        {
+          title: 'playlist recomendada',
+          url: 'https://www.youtube.com/playlist?list=PLl0IAB0xVmuS_iNXFUdYbBGdArj6dKF8o',
+          qrCodeImage: 'images/BOAS/qr-codeBOAS YT.PNG'
+        },
+      ],
+      local: 'UNITINS - ...', // comfirma o local
+      subscribeButton: 'https://classroom.google.com/u/0/c/NjY4ODk2OTU2MDYz'//colocar o link do portal quendo estiver pronto
+    },
+    ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    
+    {
+      id: 'Workshop7',
+      cardType: 'Workshop',
+      duration: '3 horas',
+      title: 'workshop para modelagem usando BPMN',
+      image: 'images/BPMN/BPMN.PNG',
+      tags: ['Gestão de fluxo', 'velocidade', 'Gestão de projetos'],
+      shortDescription: 'Nesse Workshop voce vai o que e e pra que server o BPMN.',
+      longDescription: 'Nesse Workshop você vai aprender para que server o BPMN.'
+      ,
+      instructors: [
+        {
+          name: 'Jeezyel Pereira Costa',
+          photo: 'images/user.jpg'
+        },
+        {
+          name: 'Leandro Guimarães Garcia',
+          photo: 'images/user.jpg'
+        }
+      ],
+      usefulLinks: [
+        {
+          title: 'Turma Google sala de aula',
+          url: 'https://classroom.google.com/c/NjY4ODkzNTk0NDQz',
+          qrCodeImage: 'images/BPMN/qr-code BPMN.PNG'
+        },
+      ],
+      local: 'ex: Bloco C, sala 101 colocar depos', // comfirma o local
+      subscribeButton: 'https://classroom.google.com/u/0/c/NjY4ODk2OTU2MDYz'//colocar o link do portal quendo estiver pronto
+    },
   ],
 }
 
